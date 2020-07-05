ee502e668c121330497ff8fddabe030f686b8402
JunHill <43131854+JunHill@users.noreply.github.com>
2020-07-05 22:02:52 +0700

    Add files via upload

---
+++ bai3.cpp
@@ -0,0 +1,198 @@
+// CPP Program to implement merge sort using 
+// multi-threading 
+#include <iostream> 
+#include <pthread.h> 
+#include <time.h> 
+  
+// number of elements in array 
+#define MAX 10000000
+  
+// number of threads 
+#define THREAD_MAX 4
+  
+using namespace std; 
+  
+// array of size MAX 
+int a[MAX];
+int aa[MAX];
+int part = 0; 
+  
+// merge function for merging two parts 
+void merge(int low, int mid, int high) 
+{ 
+    int* left = new int[mid - low + 1]; 
+    int* right = new int[high - mid]; 
+  
+    // n1 is size of left part and n2 is size 
+    // of right part 
+    int n1 = mid - low + 1, n2 = high - mid, i, j; 
+  
+    // storing values in left part 
+    for (i = 0; i < n1; i++) 
+        left[i] = a[i + low]; 
+  
+    // storing values in right part 
+    for (i = 0; i < n2; i++) 
+        right[i] = a[i + mid + 1]; 
+  
+    int k = low; 
+    i = j = 0; 
+  
+    // merge left and right in ascending order 
+    while (i < n1 && j < n2) { 
+        if (left[i] <= right[j]) 
+            a[k++] = left[i++]; 
+        else
+            a[k++] = right[j++]; 
+    } 
+  
+    // insert remaining values from left 
+    while (i < n1) { 
+        a[k++] = left[i++]; 
+    } 
+  
+    // insert remaining values from right 
+    while (j < n2) { 
+        a[k++] = right[j++]; 
+    } 
+} 
+
+void merge_seq(int low, int mid, int high) 
+{ 
+    int* left = new int[mid - low + 1]; 
+    int* right = new int[high - mid]; 
+  
+    // n1 is size of left part and n2 is size 
+    // of right part 
+    int n1 = mid - low + 1, n2 = high - mid, i, j; 
+  
+    // storing values in left part 
+    for (i = 0; i < n1; i++) 
+        left[i] = aa[i + low]; 
+  
+    // storing values in right part 
+    for (i = 0; i < n2; i++) 
+        right[i] = aa[i + mid + 1]; 
+  
+    int k = low; 
+    i = j = 0; 
+  
+    // merge left and right in ascending order 
+    while (i < n1 && j < n2) { 
+        if (left[i] <= right[j]) 
+            aa[k++] = left[i++]; 
+        else
+            aa[k++] = right[j++]; 
+    } 
+  
+    // insert remaining values from left 
+    while (i < n1) { 
+        aa[k++] = left[i++]; 
+    } 
+  
+    // insert remaining values from right 
+    while (j < n2) { 
+        aa[k++] = right[j++]; 
+    } 
+} 
+
+// merge sort function 
+void merge_sort(int low, int high) 
+{ 
+    // calculating mid point of array 
+    int mid = low + (high - low) / 2; 
+    if (low < high) { 
+  
+        // calling first half 
+        merge_sort(low, mid); 
+  
+        // calling second half 
+        merge_sort(mid + 1, high); 
+  
+        // merging the two halves 
+        merge(low, mid, high); 
+    } 
+}
+
+void merge_sort_seq(int low, int high) 
+{ 
+    // calculating mid point of array 
+    int mid = low + (high - low) / 2; 
+    if (low < high) { 
+  
+        // calling first half 
+        merge_sort_seq(low, mid); 
+  
+        // calling second half 
+        merge_sort_seq(mid + 1, high); 
+  
+        // merging the two halves 
+        merge_seq(low, mid, high); 
+    } 
+}  
+  
+// thread function for multi-threading 
+void* merge_sort(void* arg) 
+{ 
+    // which part out of 4 parts 
+    int thread_part = part++; 
+  
+    // calculating low and high 
+    int low = thread_part * (MAX / 4); 
+    int high = (thread_part + 1) * (MAX / 4) - 1; 
+  
+    // evaluating mid point 
+    int mid = low + (high - low) / 2; 
+    if (low < high) { 
+        merge_sort(low, mid); 
+        merge_sort(mid + 1, high); 
+        merge(low, mid, high); 
+    } 
+} 
+  
+// Driver Code 
+int main() 
+{ 
+    // generating random values in array 
+    for (int i = 0; i < MAX; i++){
+        int k = rand() % 100;
+        a[i] = k;
+        aa[i] = k;
+    } 
+  
+    // t1 and t2 for calculating time for 
+    // merge sort 
+    struct timespec t1, t2, t3; 
+  
+    clock_gettime(CLOCK_MONOTONIC, &t1); 
+    pthread_t threads[THREAD_MAX]; 
+  
+    // creating 4 threads 
+    for (int i = 0; i < THREAD_MAX; i++) 
+        pthread_create(&threads[i], NULL, merge_sort, 
+                                        (void*)NULL); 
+  
+    // joining all 4 threads 
+    for (int i = 0; i < 4; i++) 
+        pthread_join(threads[i], NULL);
+  
+    // merging the final 4 parts 
+    merge(0, (MAX / 2 - 1) / 2, MAX / 2 - 1); 
+    merge(MAX / 2, MAX/2 + (MAX-1-MAX/2)/2, MAX - 1); 
+    merge(0, (MAX - 1)/2, MAX - 1); 
+  
+    clock_gettime(CLOCK_MONOTONIC, &t2);  
+
+    merge_sort_seq(0, MAX-1);
+
+    clock_gettime(CLOCK_MONOTONIC, &t3); 
+  
+    // time taken by merge sort in seconds 
+    cout << "MERGE SORT WITH " << MAX << " int" << endl;
+    cout << "Sequential merge sort taken: " << (t3.tv_sec - t2.tv_sec) + (t3.tv_nsec - t2.tv_nsec)/1000000000 << endl; 
+
+    cout << "Parallel merge sort taken: " << (t2.tv_sec - t1.tv_sec) + (t2.tv_nsec - t1.tv_nsec)/1000000000 << endl; 
+    
+
+    return 0; 
+}
